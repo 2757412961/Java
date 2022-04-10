f74a69255d9047d5f389ac28ea972074d30a9154
2757412961 <39891566+2757412961@users.noreply.github.com>
2022-04-10 21:57:49 +0800

    2022-04-10

---
+++ src/main/java/edu/zju/zjh/lc/dp/Q0322.java
@@ -0,0 +1,36 @@
+package edu.zju.zjh.lc.dp;
+
+/**
+ * @author: zjh
+ * @date : 2022/4/10 下午 05:14
+ * @Email : 2757412961@qq.com
+ * @update:
+ */
+
+public class Q0322 {
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = amount + 1;
+        }
+
+        for (int j = 0; j < coins.length; j++) {
+            if (coins[j] <= amount) {
+                dp[coins[j]] = 1;
+            }
+        }
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] > 0) {
+                    dp[i] = Math.min(dp[i - coins[j]] + 1, dp[i]);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+}
---
+++ src/main/java/edu/zju/zjh/lc/offer/O0041.java
@@ -0,0 +1,48 @@
+package edu.zju.zjh.lc.offer;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * @author: zjh
+ * @date : 2022/4/10 下午 04:20
+ * @Email : 2757412961@qq.com
+ * @update:
+ */
+
+public class O0041 {
+
+    class MedianFinder {
+
+        private  Queue<Integer> max;
+        private  Queue<Integer> min;
+
+        /**
+         * initialize your data structure here.
+         */
+        public MedianFinder() {
+            max = new PriorityQueue<>(Collections.reverseOrder());
+            min = new PriorityQueue<>();
+        }
+
+        public void addNum(int num) {
+            if (max.size() == min.size()) {
+                min.offer(num);
+                max.offer(min.poll());
+            } else {
+                max.offer(num);
+                min.offer(max.poll());
+            }
+        }
+
+        public double findMedian() {
+            if (max.size() == min.size()) {
+                return 1.0 / 2 * (max.peek() + min.peek());
+            } else {
+                return max.peek();
+            }
+        }
+    }
+
+}
