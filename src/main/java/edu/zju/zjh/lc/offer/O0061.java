7985b545ec687a7d78f642648084ec325d01bee7
2757412961 <39891566+2757412961@users.noreply.github.com>
2022-04-23 20:55:47 +0800

    2022-04-23

---
+++ src/main/java/edu/zju/zjh/lc/offer/O0061.java
@@ -0,0 +1,31 @@
+package edu.zju.zjh.lc.offer;
+
+import java.util.Arrays;
+
+/**
+ * @author: zjh
+ * @date : 2022/4/23 下午 08:26
+ * @Email : 2757412961@qq.com
+ * @update:
+ */
+
+public class O0061 {
+
+    public boolean isStraight(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0, sum = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 0) {
+                count++;
+            } else {
+                int a = nums[i + 1] - nums[i] - 1;
+                if (a<0) return false;
+                sum += a;
+            }
+        }
+
+        return count == sum;
+    }
+
+}
