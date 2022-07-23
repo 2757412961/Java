a49348168f0b79b81c8f97c78a03a48033977625
2757412961 <39891566+2757412961@users.noreply.github.com>
2022-07-23 12:51:55 +0800

    蔚来数字化业务一面

---
+++ src/main/java/edu/zju/zjh/topk/蔚来/d20220723/Q001.java
@@ -0,0 +1,16 @@
+package edu.zju.zjh.topk.蔚来.d20220723;
+
+/**
+ * @author: zjh
+ * @date : 2022/7/23 上午 10:56
+ * @Email : 2757412961@qq.com
+ * @update:
+ */
+
+public class Q001 {
+
+    /**
+     * 全排列
+     */
+
+}
